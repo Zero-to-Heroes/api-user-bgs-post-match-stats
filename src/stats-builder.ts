2fb8b391654668cbc21ed074647a7642532881ab
sebastientromp <sebastien.tromp@gmail.com>
2023-11-16 09:58:01 +0100

    update dependencies

--- src/stats-builder.ts
+++ src/stats-builder.ts
@@ -8,53 +8,57 @@ import { Input } from './sqs-event';
 const statsConfig: { [statName: string]: (postMatchStats: BgsPostMatchStats, input: Input) => number } = {
 	totalDamageDealtToMinions: (postMatchStats: BgsPostMatchStats, input: Input): number =>
 		Object.keys(postMatchStats.totalMinionsDamageDealt)
-			.filter(cardId => cardId !== input.mainPlayer?.cardId)
-			.map(cardId => postMatchStats.totalMinionsDamageDealt[cardId])
+			.filter((cardId) => cardId !== input.mainPlayer?.cardId)
+			.map((cardId) => postMatchStats.totalMinionsDamageDealt[cardId])
 			.reduce((a, b) => a + b, 0),
 	totalDamageTakenByMinions: (postMatchStats: BgsPostMatchStats, input: Input): number =>
 		Object.keys(postMatchStats.totalMinionsDamageTaken)
-			.filter(cardId => cardId !== input.mainPlayer?.cardId)
-			.map(cardId => postMatchStats.totalMinionsDamageTaken[cardId])
+			.filter((cardId) => cardId !== input.mainPlayer?.cardId)
+			.map((cardId) => postMatchStats.totalMinionsDamageTaken[cardId])
 			.reduce((a, b) => a + b, 0),
 	totalDamageDealtToHeroes: (postMatchStats: BgsPostMatchStats, input: Input): number =>
 		postMatchStats.damageToEnemyHeroOverTurn
-			.filter(info => info.value.enemyHeroCardId !== CardIds.KelthuzadBattlegrounds)
-			.map(info => (info.value.value != null ? info.value.value : ((info.value as any) as number))) // For backward compatibility
+			.filter((info) => info.value.enemyHeroCardId !== CardIds.Kelthuzad_TB_BaconShop_HERO_KelThuzad)
+			.map((info) => (info.value.value != null ? info.value.value : (info.value as any as number))) // For backward compatibility
 			.reduce((a, b) => a + b, 0),
 	maxDamageDealtToHero: (postMatchStats: BgsPostMatchStats, input: Input): number =>
 		Math.max(
 			...postMatchStats.damageToEnemyHeroOverTurn
-				.filter(info => info.value.enemyHeroCardId !== CardIds.KelthuzadBattlegrounds)
-				.map(info => (info.value.value != null ? info.value.value : ((info.value as any) as number))), // For backward compatibility
+				.filter((info) => info.value.enemyHeroCardId !== CardIds.Kelthuzad_TB_BaconShop_HERO_KelThuzad)
+				.map((info) => (info.value.value != null ? info.value.value : (info.value as any as number))), // For backward compatibility
 		),
 	highestWinStreak: (postMatchStats: BgsPostMatchStats, input: Input): number => input.mainPlayer?.highestWinStreak,
 	triplesCreated: (postMatchStats: BgsPostMatchStats, input: Input): number => input.mainPlayer?.tripleHistory.length,
 	maxBoardStats: (postMatchStats: BgsPostMatchStats, input: Input): number =>
-		Math.max(...postMatchStats.totalStatsOverTurn.map(stat => stat.value)),
+		Math.max(...postMatchStats.totalStatsOverTurn.map((stat) => stat.value)),
 	coinsWasted: (postMatchStats: BgsPostMatchStats, input: Input): number =>
-		postMatchStats.coinsWastedOverTurn.map(value => value.value).reduce((a, b) => a + b, 0),
+		postMatchStats.coinsWastedOverTurn.map((value) => value.value).reduce((a, b) => a + b, 0),
 	rerolls: (postMatchStats: BgsPostMatchStats, input: Input): number => {
-		const rerolls = postMatchStats.rerollsOverTurn.map(value => value.value).reduce((a, b) => a + b, 0);
-		return input.mainPlayer?.cardId === CardIds.InfiniteTokiBattlegrounds
-			? rerolls - postMatchStats.mainPlayerHeroPowersOverTurn.map(value => value.value).reduce((a, b) => a + b, 0)
+		const rerolls = postMatchStats.rerollsOverTurn.map((value) => value.value).reduce((a, b) => a + b, 0);
+		return input.mainPlayer?.cardId === CardIds.InfiniteToki
+			? rerolls -
+					postMatchStats.mainPlayerHeroPowersOverTurn.map((value) => value.value).reduce((a, b) => a + b, 0)
 			: rerolls;
 	},
 	heroPowerUsed: (postMatchStats: BgsPostMatchStats, input: Input): number =>
-		postMatchStats.mainPlayerHeroPowersOverTurn.map(value => value.value).reduce((a, b) => a + b, 0),
+		postMatchStats.mainPlayerHeroPowersOverTurn.map((value) => value.value).reduce((a, b) => a + b, 0),
 	freezes: (postMatchStats: BgsPostMatchStats, input: Input): number =>
-		postMatchStats.freezesOverTurn.map(value => value.value).reduce((a, b) => a + b, 0),
+		postMatchStats.freezesOverTurn.map((value) => value.value).reduce((a, b) => a + b, 0),
 	minionsBought: (postMatchStats: BgsPostMatchStats, input: Input): number =>
-		postMatchStats.minionsBoughtOverTurn.map(value => value.value).reduce((a, b) => a + b, 0),
+		postMatchStats.minionsBoughtOverTurn.map((value) => value.value).reduce((a, b) => a + b, 0),
 	minionsSold: (postMatchStats: BgsPostMatchStats, input: Input): number =>
-		postMatchStats.minionsSoldOverTurn.map(value => value.value).reduce((a, b) => a + b, 0),
+		postMatchStats.minionsSoldOverTurn.map((value) => value.value).reduce((a, b) => a + b, 0),
 	enemyMinionsKilled: (postMatchStats: BgsPostMatchStats, input: Input): number =>
 		postMatchStats.totalEnemyMinionsKilled,
 	enemyHeroesKilled: (postMatchStats: BgsPostMatchStats, input: Input): number =>
 		postMatchStats.totalEnemyHeroesKilled,
 	percentageOfBattlesGoingFirst: (postMatchStats: BgsPostMatchStats, input: Input): number => {
-		const battlesGoingFirst = postMatchStats.wentFirstInBattleOverTurn.filter(value => value.value === true).length;
-		const battlesGoingSecond = postMatchStats.wentFirstInBattleOverTurn.filter(value => value.value === false)
-			.length;
+		const battlesGoingFirst = postMatchStats.wentFirstInBattleOverTurn.filter(
+			(value) => value.value === true,
+		).length;
+		const battlesGoingSecond = postMatchStats.wentFirstInBattleOverTurn.filter(
+			(value) => value.value === false,
+		).length;
 		return (100 * battlesGoingFirst) / (battlesGoingFirst + battlesGoingSecond);
 	},
 	battleLuck: (postMatchStats: BgsPostMatchStats, input: Input): number => 100 * postMatchStats.luckFactor,
@@ -69,7 +73,7 @@ export const buildNewStats = (
 ): readonly BgsBestStat[] => {
 	return Object.keys(statsConfig)
 		.map((statName: StatName) => handleStat(statName, existingStats, postMatchStats, input, today))
-		.filter(stat => stat);
+		.filter((stat) => stat);
 };
 
 const handleStat = (
@@ -81,7 +85,7 @@ const handleStat = (
 ): BgsBestStat => {
 	const extractor = statsConfig[statName];
 	const newStatValue: number = extractor(postMatchStats, input);
-	const existingStat = existingStats.find(stat => stat.statName === statName);
+	const existingStat = existingStats.find((stat) => stat.statName === statName);
 	const existingStatValue: number = existingStat?.value;
 	if (newStatValue && (!existingStatValue || newStatValue > existingStatValue)) {
 		const newStat: BgsBestStat = {
